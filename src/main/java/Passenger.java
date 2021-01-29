b9aeef99bd66778a6ea2d55dabbcca01cdac0102
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-29 11:52:09 +0000

    initial commit

---
+++ src/main/java/Passenger.java
@@ -0,0 +1,30 @@
+public class Passenger {
+
+    private String name;
+    private int numOfBags;
+
+
+    public Passenger(String name, int numOfBags) {
+        this.name = name;
+        this.numOfBags = numOfBags;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getNumOfBags() {
+        return numOfBags;
+    }
+
+    public void setNumOfBags() {
+        this.numOfBags = numOfBags;
+    }
+    
+}
