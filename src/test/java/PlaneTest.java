a13897d6651da075f5c2bb92495599eb6986a527
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-29 14:17:17 +0000

    Tested Pane and PlaneType

--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -5,17 +5,27 @@ import static org.junit.Assert.assertEquals;
 
 public class PlaneTest {
 
-    Plane plane;
+    Plane airBusA320;
 
     // This method will be run before each testcase to reset/initialise the data.
     @Before
     public void before() {
-        plane = new Plane(PlaneType.AIRBUSA320);
+        airBusA320 = new Plane(PlaneType.AIRBUSA320);
+    }
+
+    @Test
+    public void canGetPlaneType(){
+        assertEquals(PlaneType.AIRBUSA320, airBusA320.getPlaneType());
     }
 
     @Test
     public void canGetCapacity(){
-        assertEquals();
+        assertEquals(180, airBusA320.getCapacity());
+    }
+
+    @Test
+    public void canGetTotalWeight(){
+        assertEquals(83_000, airBusA320.getTotalWeight());
     }
 
 }
